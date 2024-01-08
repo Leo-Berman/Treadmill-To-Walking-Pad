c494f83fca410194aff315e57c55bab5ef41d882
Liam Coleman <liam@megster.com>
2024-01-08 18:52:36 -0500

    Change port to work with raspberry pi hopefully

--- site/webserver.py
+++ site/webserver.py
@@ -6,13 +6,16 @@ from flask import send_from_directory
 
 
 app = Flask(__name__)
+            # static_url_path='/', 
+            # static_folder='templates/my-app/build/',
+            # template_folder='templates/my-app/build/')
 
 @app.route("/")
 def index():
-    return render_template('index.html')
-# @app.route("/<path:path>")
-# def svelte_client(path):
-#     return send_from_directory('../svelte/public/', path)
+    return render_template('my-app/build/index.html')
+# @app.route("/")
+# def base():
+#     return send_from_directory('my-app/build/', 'index.html')
 
 
 @app.route('/speed/<int:value>')
@@ -43,14 +46,14 @@ def stop():
 
 def incline(direction: int):
     print(f"Setting incline to {direction}")
-    ser = serial.Serial('/dev/tty.usbmodem1101')
+    ser = serial.Serial('/dev/ttyS0')
     print(ser.name) 
     ser.write(f"I {direction}".encode("utf-8"))
     ser.close()
 
 def speed(speed: int):
     print(f"Setting speed to {speed}")
-    ser = serial.Serial('/dev/tty.usbmodem1101')
+    ser = serial.Serial('/dev/ttyS0')
     print(ser.name) 
     ser.write(f"S {speed}".encode("utf-8"))
     ser.close()
