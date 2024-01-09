9075597bb70831052ad104f14b22a8879f2e7058
leo-berman <leo.berman@temple.edu>
2024-01-09 15:45:23 -0500

    Fixed bug preventing string from being read

--- site/webserver.py
+++ site/webserver.py
@@ -19,24 +19,24 @@ ser = serial.Serial(
         timeout=0
         )
 
-# @app.route("/")
-# def index():
-#     return render_template('index.html')
-
 @app.route("/")
-def base():
-    return send_from_directory('my-app/build', 'index.html')
+def index():
+     return render_template('index.html')
+
+#@app.route("/")
+#def base():
+#    return send_from_directory('my-app/build', 'index.html')
 
 # Path for all the static files (compiled JS/CSS, etc.)
-@app.route("/<path:path>")
-def home(path):
-    return send_from_directory('my-app/build', path)
+#@app.route("/<path:path>")
+#def home(path):
+#    return send_from_directory('my-app/build', path)
 
 @app.route('/speed/<int:value>')
 def speed(value):
     print(f"Setting speed to {value}")
     # TODO check bounds
-    # speed(value)
+    speed(value)
     return f"{value}"
 
 @app.route("/up")
@@ -61,12 +61,14 @@ def incline(direction: int):
     print(f"Setting incline to {direction}")
     # ser = serial.Serial('/dev/AMC0')
     print(ser.name) 
-    ser.write(f"I {direction}".encode("utf-8"))
+    ser.write(f"I {direction}\n".encode("utf-8"))
+    print(f"I {direction}\n".encode("utf-8"))
     # ser.close()
 
 def speed(speed: int):
     print(f"Setting speed to {speed}")
     # ser = serial.Serial('/dev/AMC0')
     print(ser.name) 
-    ser.write(f"S {speed}".encode("utf-8"))
+    ser.write(f"S {speed}\n".encode("utf-8"))
+    print(f"S {speed}\n".encode("utf-8"))
     # ser.close()
